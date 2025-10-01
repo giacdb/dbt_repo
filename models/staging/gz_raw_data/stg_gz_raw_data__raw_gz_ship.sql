97deb51200853e84b0302db211279e223615fd1d
giacomo.d.bono@gmail.com <giacomo.d.bono@gmail.com>
2025-10-01 08:52:24 +0000

    task 3

---
+++ models/staging/gz_raw_data/stg_gz_raw_data__raw_gz_ship.sql
@@ -0,0 +1,22 @@
+with 
+
+source as (
+
+    select * from {{ source('gz_raw_data', 'raw_gz_ship') }}
+
+),
+
+  renamed as (
+
+      select
+          orders_id,
+          shipping_fee,
+          logcost AS log_cost,
+          CAST(ship_cost AS INT64) AS ship_cost
+
+      from source
+
+  )
+
+
+select * from renamed
