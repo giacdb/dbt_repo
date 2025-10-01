249fa9fed6ec414ebc97a6d0846dee1ad0dd4d95
Giacomo Del Bono <giacomo.d.bono@gmail.com>
2025-10-01 13:57:47 +0000

    task 3

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -2,7 +2,7 @@ with
 
 source as (
 
-    select * from {{ source('gz_raw_data', 'raw_gz_sales') }}
+    select * from {{ source('raw', 'sales') }}
 
 ),
 
@@ -11,7 +11,7 @@ renamed as (
     select
         date_date,
         orders_id,
-        pdt_id AS products_id,
+        pdt_id,
         revenue,
         quantity
 
